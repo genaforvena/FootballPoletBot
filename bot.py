5e165c79435d88010aad1f4f6fb67831541fb3fe
Ilya Mozerov <ilyailiilia@gmail.com>
2016-01-20 20:05:10 +0300

    Extracted method dispatch_message
    Added try-catch block

--- bot.py
+++ bot.py
@@ -9,6 +9,96 @@ from utils.markup_utils import generate_plus_minus_markup
 
 bot = telebot.TeleBot(config.token)
 
+
+def dispatch_message_and_respond(all_users, match, message, player, store):
+    if "да матч" in message.text or "да игра" in message.text:
+        response = match.annotate()
+        if player.telegram_id not in match.players.keys():
+            markup = generate_plus_minus_markup()
+            bot.send_message(message.chat.id, response, reply_markup=markup)
+        else:
+            bot.send_message(message.chat.id, response)
+
+    elif message.text == "+" or message.text == "Я +" or message.text == "я +" or message.text == "плюс":
+        match.add_player(player)
+        send_message_to(match.players.values(), "Нас " + str(match.players_number()))
+
+    elif message.text == "-" or message.text == "Я -" or message.text == "я -" or message.text == "минус":
+        match.remove_player(player)
+        response = "("
+        bot.send_message(message.chat.id, response)
+        if player.telegram_id in match.players.keys() and match.players_number() < 10:
+            send_message_to(match.players.values(), "Один -. Нас теперь " + str(match.players_number()))
+
+    elif "мной +" in message.text:
+        match.add_guests(player, 1)
+        send_message_to(match.players.values(), "Нас " + str(match.players_number()))
+
+    elif "мной -" in message.text:
+        match.remove_guest(player)
+        response = "((("
+        bot.send_message(message.chat.id, response)
+        if match.players_number() < 10:
+            send_message_to(match.players.values(), "Один -. Нас теперь " + str(match.players_number()))
+
+    elif "ы игра" in message.text:
+        if match.players_number() >= 10:
+            response = "Да \nНас " + str(match.players_number())
+        else:
+            response = "Нас пока " + str(match.players_number())
+        bot.send_message(message.chat.id, response)
+
+    elif "Создать матч" in message.text:
+        match = Match("Полет", "Понедельник 20-30")
+        store["match"] = match
+        response = "Следующий матч: \n" + match.annotate() + "\n\nИдешь?"
+        send_message_to(all_users.values(), response, generate_plus_minus_markup())
+        bot.send_message(message.chat.id, response)
+
+    elif "то ид" in message.text or "олько на" in message.text:
+        response = "Нас " + str(match.players_number()) + "\nТочно идут: \n" + match.annotate_players()
+        bot.send_message(message.chat.id, response)
+
+    elif "Прив" in message.text or "Как дела?" in message.text:
+        response = "Иди нахуй"
+        bot.send_message(message.chat.id, response)
+
+    elif "дрес" in message.text:
+        response = "ул. Чаадаева, 20А"
+        bot.send_message(message.chat.id, response)
+
+    elif "дали меня" in message.text:
+        try:
+            del all_users[player.telegram_id]
+            response = "Больше никаких уведомлений на твой номер."
+            if player.telegram_id in match.players.keys():
+                match.remove_player(player)
+                if match.players_number() < 10:
+                    send_message_to(match.players.values(), "Один -. Нас теперь " + str(match.players_number()))
+        except:
+            # i know that this is bad but
+            pass
+        bot.send_message(message.chat.id, response)
+
+    elif "то подписан" in message.text:
+        response = "На уведомления подписаны:\n"
+        for user in all_users.values():
+            response += user.name + "\n"
+        bot.send_message(message.chat.id, response)
+
+    else:
+        response = "Я понимаю команды:" \
+                   "\nКогда матч?" \
+                   "\nКакой адрес?" \
+                   "\n+" \
+                   "\n-" \
+                   "\nСо мной +" \
+                   "\nСо мной -" \
+                   "\nКто идет?" \
+                   "\nУдали меня из списка уведомлений"
+        bot.send_message(message.chat.id, response)
+
+
 @bot.message_handler(func=lambda message: True, content_types=['text'])
 def check_answer(message):
     with shelve.open(config.shelve_file, writeback=True) as store:
@@ -20,80 +110,13 @@ def check_answer(message):
             all_users = store["users"]
         except:
             all_users = {}
+
         player = Player(message.chat.id, message.chat.first_name + " " + message.chat.last_name)
         all_users[player.telegram_id] = player
         store["users"] = all_users
-        markup = None
-        if "да матч" in message.text or "да игра" in message.text:
-            response = match.annotate()
-            if player.telegram_id not in match.players.keys():
-                markup = generate_plus_minus_markup()
-        elif message.text == "+" or message.text == "Я +" or message.text == "я +" or message.text == "плюс":
-            match.add_player(player)
-            response = "Нас " + str(match.players_number())
-            if match.players_number() == 10:
-                send_message_to(match.players.values(), "Нас точно 10, играем")
-        elif message.text == "-" or message.text == "Я -" or message.text == "я -" or message.text == "минус":
-            match.remove_player(player)
-            response = "("
-            if player.telegram_id in match.players.keys() and match.players_number() < 10:
-                send_message_to(match.players.values(), "Один -. Нас теперь " + str(match.players_number()))
-        elif "мной +" in message.text:
-            match.add_guests(player, 1)
-            response = "Нас " + str(match.players_number())
-            if match.players_number() == 10:
-                send_message_to(match.players.values(), "Нас точно 10, играем")
-        elif "мной -" in message.text:
-            match.remove_guest(player)
-            response = "((("
-            if match.players_number() < 10:
-                send_message_to(match.players.values(), "Один -. Нас теперь " + str(match.players_number()))
-        elif "ы игра" in message.text:
-            if match.players_number() >= 10:
-                response = "Да \nНас " + str(match.players_number())
-            else:
-                response = "Нас пока " + str(match.players_number())
-        elif "Создать матч" in message.text:
-            match = Match("Полет", "Понедельник 20-30")
-            store["match"] = match
-            response = "Следующий матч: \n" + match.annotate() + "\n\nИдешь?"
-            send_message_to(all_users.values(), response, generate_plus_minus_markup())
-        elif "то ид" in message.text or "олько на" in message.text:
-            response = "Нас " + str(match.players_number()) + "\nТочно идут: \n" + match.annotate_players()
-        elif "Прив" in message.text or "Как дела?" in message.text:
-            response = "Иди нахуй"
-        elif "дрес" in message.text:
-            response = "ул. Чаадаева, 20А"
-        elif "дали меня" in message.text:
-            try:
-                del all_users[player.telegram_id]
-                response = "Больше никаких уведомлений на твой номер."
-                if player.telegram_id in match.players.keys():
-                    match.remove_player(player)
-                    if match.players_number() < 10:
-                        send_message_to(match.players.values(), "Один -. Нас теперь " + str(match.players_number()))
-            except:
-                # i know that this is bad but
-                pass
-        elif "то подписан" in message.text:
-            response = "На уведомления подписаны:\n"
-            for user in all_users.values():
-                response += user.name + "\n"
-        else:
-            response = "Я понимаю команды:" \
-                       "\nКогда матч?" \
-                       "\nКакой адрес?" \
-                       "\n+" \
-                       "\n-" \
-                       "\nСо мной +" \
-                       "\nСо мной -" \
-                       "\nКто идет?" \
-                       "\nУдали меня из списка уведомлений"
+
+        dispatch_message_and_respond(all_users, match, message, player, store)
         store.sync()
-    if markup is None:
-        bot.send_message(message.chat.id, response)
-    else:
-        bot.send_message(message.chat.id, response, reply_markup=markup)
 
 
 def send_message_to(players, message, markup = None):
@@ -105,5 +128,8 @@ def send_message_to(players, message, markup = None):
 
 if __name__ == '__main__':
     while True:
-        bot.polling(timeout=120, none_stop=True)
-        time.sleep(200)
+        try:
+            bot.polling(timeout=120, none_stop=True)
+        except:
+            pass
+        time.sleep(600)
